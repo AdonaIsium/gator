6ad2eb167ea385601d963eb1392a2bb2ea98af9d
Lanre <lanre.arisen@gmail.com>
2025-09-07 15:54:43 -0500

    pre-refactor

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,27 +3,33 @@ package internal
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
-const configFileName = "~/.gatorconfig.json"
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
 	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() (*Config, error) {
-	data, err := os.ReadFile(configFileName)
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
 	if err != nil {
-		return nil, err
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(fullPath)
+	if err != nil {
+		return Config{}, err
 	}
 
 	var cfg Config
 	if err := json.Unmarshal(data, &cfg); err != nil {
-		return nil, err
+		return Config{}, err
 	}
 
-	return &cfg, nil
+	return cfg, nil
 }
 
 func (cfg *Config) SetUser(username string) error {
@@ -42,11 +48,19 @@ func write(cfg Config) error {
 	if err != nil {
 		return err
 	}
-	os.WriteFile(configFileName, data, 0644)
-
-	return nil
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fullPath, data, 0644)
 }
 
 func getConfigFilePath() (string, error) {
-	return configFileName, nil
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	fullPath := filepath.Join(home, configFileName)
+	return fullPath, nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"github.com/AdonaIsium/gator/internal"
+	"fmt"
+	"log"
+
+	config "github.com/AdonaIsium/gator/internal/config"
 )
 
 func main() {
-	internal.Read()
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+
+	err = cfg.SetUser("Lanre")
+	if err != nil {
+		log.Fatalf("error setting username: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+	fmt.Printf("db_url: %s, current_user_name: %s", cfg.DBURL, cfg.CurrentUserName)
 }
